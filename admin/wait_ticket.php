66659bfe7ce413ff7ef14032385e7eccf40111e4
nickryan07 <nickryan07@sbcglobal.net>
2018-04-02 14:39:49 -0500

    Wait Tickets
    Create wait tickets

---
+++ admin/wait_ticket.php
@@ -0,0 +1,243 @@
+<?php
+/*
+ *   CC BY-NC-AS UTA FabLab 2016-2017
+ *   FabApp V 0.9
+ */
+include_once ($_SERVER['DOCUMENT_ROOT'].'/pages/header.php');
+$d_id = $dg_id = $operator = "";
+
+if (!$staff || $staff->getRoleID() < 7){
+    //Not Authorized to see this Page
+    header('Location: /index.php');
+	exit();
+}
+?>
+<title><?php echo $sv['site_name'];?> Create Wait Ticket</title>
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Create Wait Ticket</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-md-6">
+            <?php if ($staff && $staff->getRoleID() >= $sv['minRoleTrainer']) {?>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <i class="fas fa-ticket-alt" aria-hidden="true"></i> Create New Wait Ticket
+                    </div>
+                    <div class="panel-body">
+                        <table class="table table-bordered table-striped table-hover"><form name="tcForm" id="tcForm" autocomplete="off" method="POST" action="">
+                            <tr>
+                                <td><a href="#" data-toggle="tooltip" data-placement="top" title="Which device does this wait ticket belong to?">Select Device or Group</a></td>
+                                <td>
+                                    <select name="d_id" id="d_id" onchange="selectDevice(this)" tabindex="1">
+                                        <option disabled hidden selected value="">Device</option>
+                                        <?php if($result = $mysqli->query("
+                                            SELECT DISTINCT `devices`.`d_id`, `devices`.`device_desc`
+                                            FROM `devices`
+                                            INNER JOIN `device_group`
+                                            ON `devices`.`dg_id` = `device_group`.`dg_id`
+                                            WHERE `granular_wait` = 'Y'
+                                            ORDER BY `device_desc`
+                                        ")){
+                                            while($row = $result->fetch_assoc()){
+                                                echo("<option value='$row[d_id]'>$row[device_desc]</option>");
+                                            }
+                                        } else {
+                                            echo ("Device list Error - SQL ERROR");
+                                        }?>
+                                    </select> or <select name="dg_id" id="dg_id" onchange="selectDevice(this)" tabindex="2">
+                                        <option disabled hidden selected value="">Device Group</option>
+                                        <?php if($result = $mysqli->query("
+                                            SELECT DISTINCT `device_group`.`dg_id`, `device_group`.`dg_desc`
+                                            FROM `device_group`
+                                            WHERE `granular_wait` = 'N'
+                                            ORDER BY `dg_desc`
+                                        ")){
+                                            while($row = $result->fetch_assoc()){
+                                                echo("<option value='$row[dg_id]'>$row[dg_desc]</option>");
+                                            }
+                                        } else {
+                                            echo ("Device list Error - SQL ERROR");
+                                        }?>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><a href="#" data-toggle="tooltip" data-placement="top" title="The email of the person that you will issue a wait ticket for">(Optional) Email</a></td>
+                                <td><input type="text" name="op-email" id="op-email" class="form-control" placeholder="email" maxlength="100" size="10"/></td>
+                            </tr>
+                            <tr>
+                                <td><a href="#" data-toggle="tooltip" data-placement="top" title="The phone number of person that you will issue a wait ticket for">(Optional) Phone</a></td>
+                                <td><input type="text" name="op-phone" id="op-phone" class="form-control" placeholder="phone number" maxlength="10" size="10"/></td>
+                            </tr>
+                            <tr>
+                                <td><a href="#" data-toggle="tooltip" data-placement="top" title="The person that you will issue a wait ticket for">Operator</a></td>
+                                <td><input type="text" name="operator" id="operator" class="form-control" placeholder="1000000000" maxlength="10" size="10"/></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label><i class="fa fa-info-circle"></i> Disclaimer</label>
+                                </td>
+                                <td>
+                                    <div class="form-group">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" name="disclaimer" value="">I have read and understand the <a>FERPA release policies</a>.
+                                            </label>
+                                        </div>
+                                        <?php
+                                        if(isset($_POST['disclaimer'])) {
+                                            if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submitBtn'])) {
+                                                $operator = filter_input(INPUT_POST, 'operator');
+                                                /*if(isset($device)) {
+                                                    $wait_id = Wait_queue::insertWaitQueue($operator, $device->getD_id(), $est_time, $p_id, $status_id, $staff);
+                                                } else if(isset($device, NULL, 2)) {
+                                                    $wait_id = 
+                                                }*/
+                                                $d_id = filter_input(INPUT_POST,'d_id');
+                                                $dg_id = filter_input(INPUT_POST,'dg_id');
+                                                $em = filter_input(INPUT_POST,'op-email');
+                                                $ph = filter_input(INPUT_POST, 'op-phone');
+                                                $wait_id = Wait_queue::insertWaitQueue($operator, $d_id, $dg_id, $ph, $em);
+
+                                                //$msg = false;
+
+                                                //$msg = submitTM($tm_id, $operator, $staff);
+
+                                                //if ($msg === true){
+                                                 //   $_SESSION['type'] = 'tc_success';
+                                                //    $_SESSION['tm_id'] = $tm_id;
+                                                 //   header("Location:wait_ticket.php");
+                                                //} else {
+                                                 //   echo "<script type='text/javascript'> window.onload = function(){goModal('Error',\"$msg\", false)}//</script>";
+                                                //}
+                                            }
+                                        } else {
+                                            echo ("You must accept the disclaimer if you enter contact information.");
+                                        }
+                                        ?>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tfoot>
+                                <tr>
+                                    <td colspan="2"><div class="pull-right"><input type="submit" name="submitBtn" value="Submit"></div></td>
+                                </tr>
+                            </tfoot>
+                        </form></table>
+                    </div>
+                    <!-- /.panel-body -->
+                </div>
+                <!-- /.panel -->
+            <?php } elseif($staff) { ?>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <i class="fas fa-sign-in-alt fa-lg"></i>  Issue wait ticket
+                    </div>
+                    <div class="panel-body">
+                        <?php
+                            echo ("To issue a wait ticket, you must be logged in as ".ROLE::getTitle($sv['minRoleTrainer'])." or higher.");
+                        ?>
+                    </div>
+                    <!-- /.panel-body -->
+                </div>
+                <!-- /.panel -->
+            <?php } else { ?>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <i class="fas fa-sign-in-alt fa-lg"></i> Please Log In
+                    </div>
+                    <div class="panel-body">
+                    </div>
+                    <!-- /.panel-body -->
+                </div>
+                <!-- /.panel -->
+            <?php } ?>
+        </div>
+    </div>
+    <!-- /.row -->
+</div>
+<!-- /#page-wrapper -->
+
+<?php
+//Standard call for dependencies
+include_once ($_SERVER['DOCUMENT_ROOT'].'/pages/footer.php');
+?>
+<script type="text/javascript">
+    /*
+    $('#teTable').DataTable({
+        searching: false, 
+        paging: false});
+    
+    //AJAX call to build a list of training modules for the specified device or device group
+    function selectDevice(element){
+        if (element.id == 'd_id'){
+            document.getElementById("dg_id").selectedIndex = 0;
+        } else if (element.id == 'dg_id') {
+            document.getElementById("d_id").selectedIndex = 0;
+        }
+        document.getElementById("tm_id").selectedIndex = 0;
+        document.getElementById("td_deviceList").innerHTML = "";
+
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                //document.getElementById("tr_tm").innerHTML = this.responseText;
+                document.getElementById("tm_id").innerHTML = this.responseText;
+                document.getElementById("tm_desc").innerHTML = "";
+            }
+        };
+        device = element.id + "=" + element.value;
+        xmlhttp.open("GET","sub/certTM.php?" + device,true);
+        xmlhttp.send();
+        
+        //List Devices
+        if (element.id == 'dg_id') {
+            if (window.XMLHttpRequest) {
+                // code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp2 = new XMLHttpRequest();
+            } else {
+                // code for IE6, IE5
+                xmlhttp2 = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp2.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    //document.getElementById("tr_tm").innerHTML = this.responseText;
+                    document.getElementById("td_deviceList").innerHTML = this.responseText;
+                }
+            };
+            device = element.id + "=" + element.value;
+            xmlhttp2.open("GET","sub/certDevices.php?" + device,true);
+            xmlhttp2.send();
+        }
+    }
+    
+    
+    //AJAX call to build a list of training modules for the specified device or device group
+    function getDesc(element){
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("tm_desc").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","sub/descTM.php?tm_id=" + element.value,true);
+        xmlhttp.send();
+    }*/
+</script>
---
+++ class/Wait_queue.php
@@ -0,0 +1,203 @@
+<?php
+
+
+class Wait_queue {
+    private $device;
+    private $device_group;
+    private $start_time;
+    private $end_time;
+    private $wait_id;
+    private $valid;
+    
+    public function __construct($q_id){
+        global $mysqli;
+        
+        if (!preg_match("/^\d+$/", $q_id))
+            throw new Exception("Invalid Ticket Number : $q_id");
+        
+        if ($result = $mysqli->query("
+            SELECT *
+            FROM wait_queue
+            WHERE Q_id = $q_id
+            LIMIT 1;
+        ")){
+            if ($result->num_rows == 0 ){
+                throw new Exception("Ticket Not Found : $q_id");
+            }
+            $row = $result->fetch_assoc();
+            $this->setWaitId($row['Q_id']);
+            $this->setDevId($row['Dev_id']);
+            $this->setDevgrId($row['Devgr_id']);
+            $this->setStartTime($row['start_date']);
+            $this->setEndTime($row['end_date']);
+            $this->setValid($row['valid']);
+        }
+        
+    }
+    
+
+    public static function insertWaitQueue($operator, $d_id, $dg_id, $phone, $email) {
+        global $mysqli;
+        
+        /**
+         * TODO: variable validation
+         * d_id, dg_id
+         */
+        
+        //Validate input variables
+		if (!self::regexPhone($phone) && !empty($phone)) {
+            echo ("Bad phone number - ");
+            echo $phone;
+            return "Bad phone number";
+        }
+        
+        if (!self::regexOperator($operator)) {
+            echo ("Bad operator number - ");
+            echo $operator;
+            return "Bad operator number";
+        }
+		
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL) && !empty($email)) {
+            echo ("Bad email - ");
+            echo $email;
+			return "Bad email";
+		}
+        
+        if(isset($d_id)) {
+            if ($mysqli->query("
+            INSERT INTO operator_info
+                (`Op_id`, `Op_email`, `Op_phone`, `last_contact`)
+            VALUES
+                ('$operator', '$email', '$phone', CURRENT_TIMESTAMP);
+            ")){
+                echo ("\nSuccessfully inserted ticket!");
+                //insertContactInfo($operator, $phone, $email);
+                if ($mysqli->query("
+                INSERT INTO wait_queue 
+                  (`operator`,`dev_id`,`start_date`) 
+                VALUES
+                    ('$operator','$d_id', CURRENT_TIMESTAMP);
+            ")){
+
+                    self::sendNotification($phone, "Fabapp Notification", "You have signed up for fabapp notifications. Your ticket number is: ".$mysqli->insert_id."", 'From: Fabapp Notifications' . "\r\n" .'');
+                echo ("\nSuccessfully updated contact info!");
+                return $mysqli->insert_id;
+            } else {
+                echo ("Error updating contact info!");
+                return $mysqli->error;
+            }
+                return $mysqli->insert_id;
+            } else {
+                echo ("\nError updating ticket!");
+                return $mysqli->error;
+            }
+        } else if(isset($dg_id)) {
+            if ($mysqli->query("
+            INSERT INTO operator_info
+                (`Op_id`, `Op_email`, `Op_phone`, `last_contact`)
+            VALUES
+                ('$operator', '$email', '$phone', CURRENT_TIMESTAMP);
+            ")){
+                echo ("\nSuccessfully inserted ticket!");
+                //insertContactInfo($operator, $phone, $email);
+                if ($mysqli->query("
+                INSERT INTO wait_queue 
+                  (`operator`,`devgr_id`,`start_date`) 
+                VALUES
+                    ('$operator','$dg_id', CURRENT_TIMESTAMP);
+                ")){
+
+                    self::sendNotification($phone, "Fabapp Notification", "You have signed up for fabapp notifications. Your ticket number is: ".$mysqli->insert_id."", "");
+                    echo ("\nSuccessfully updated contact info!");
+                    return $mysqli->insert_id;
+                } else {
+                    echo ("Error updating contact info!");
+                    return $mysqli->error;
+                }
+                    return $mysqli->insert_id;
+            } else {
+                echo ("\nError updating ticket!");
+                return $mysqli->error;
+            }
+        }
+    }
+
+    public static function insertContactInfo($operator, $op_phone, $op_email) {
+        global $mysqli;
+
+        //Validate input variables
+		if (!self::regexPhone($op_phone)) {
+            echo ("Bad phone number - ");
+            echo $op_phone;
+            return "Bad phone number";
+        }
+		
+		if(!filter_var($op_email, FILTER_VALIDATE_EMAIL)) {
+            echo ("Bad email");
+			return "Bad email";
+		}
+        /**
+         * TODO: Use IF EXISTS and to update if an operator already exists in the table
+         */
+        if ($mysqli->query("
+            INSERT INTO operator_info
+                (`Op_id`, `Op_email`, `Op_phone`, `last_contact`)
+            VALUES
+                ('$operator', '$op_email', '$op_phone', CURRENT_TIMESTAMP);
+        ")){
+            
+            echo ("\nSuccessfully updated contact info!");
+            return $mysqli->insert_id;
+        } else {
+            echo ("Error updating contact info!");
+            return $mysqli->error;
+        }
+    }
+    
+    
+    //Probaby needs to be a class
+    public static function sendNotification($phone, $subject, $message, $headers) {
+        // This function needs to query the carrier table and send an email to all combinations
+        if(mail("".$phone."@tmomail.net", $subject, $message, $headers))
+            echo "Email sent";
+        else
+            echo "Email sending failed";
+    }
+
+    public static function regexPhone($phone) {
+        if ( preg_match("/^\d{10}$/", $phone) == 1 )
+            return true;
+        return false;
+    }
+    
+    public static function regexOperator($op_id) {
+        if ( preg_match("/^\d{10}$/", $op_id) == 1 )
+            return true;
+        return false;
+    }
+    
+    public function setWaitId($q_id) {
+        $this->wait_id = $q_id;
+    }
+
+    public function setValid($op_id) {
+        $this->valid = $valid;
+    }
+
+    public function setStartTime($start_time) {
+        $this->start_time = $start_time;
+    }
+
+    private function setEndTime($end_time) {
+        $this->end_time = $end_time;
+    }
+
+    private function setDevId($d_id) {
+        $this->device = $d_id;
+    }
+
+    private function setDevgrId($dg_id) {
+        $this->device_group = $dg_id;
+    }
+}
+?>
